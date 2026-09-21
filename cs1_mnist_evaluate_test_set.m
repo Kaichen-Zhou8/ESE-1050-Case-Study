d7b4a7f3b43240a8b818eb9c73025bbb0da0a32a
Kaichen-Zhou8 <zhouk@wustl.edu>
2026-09-21 11:22:18 -0500

    Started the Test Set

--- cs1_mnist_evaluate_test_set.m
+++ cs1_mnist_evaluate_test_set.m
@@ -6,15 +6,20 @@
 % 2)'centroids' be established in the workspace
 % AND
 % 3)'test' be established in the workspace
+% testing set (200 images with 11 outliers)
+test=csvread('mnist_test_200.csv');
+% store the correct test labels
+correctlabels = test(:,785);
+test=test(:,1:784);
 
 
 % IMPORTANT!!:
 % You should save 1) and 2) in a file named 'classifierdata.mat' as part of
 % your submission.
-
+load ('classifierdata.mat');
 predictions = zeros(200,1);
 outliers = zeros(200,1);
-
+distances = zeros(200,1);
 % loop through the test set, figure out the predicted number
 for i = 1:200
 
@@ -24,13 +29,15 @@ testing_vector=test(i,:);
 [prediction_index, vec_distance]=assign_vector_to_centroid(testing_vector,centroids);
 
 predictions(i) = centroid_labels(prediction_index);
+distances(i) = vec_distance;
 
 end
+disp("Average distance to closest centroid: " + mean(distances));
 
 %% DESIGN AND IMPLEMENT A STRATEGY TO SET THE outliers VECTOR
 % outliers(i) should be set to 1 if the i^th entry is an outlier
 % otherwise, outliers(i) should be 0
-% FILL IN
+
 
 %% MAKE A STEM PLOT OF THE OUTLIER FLAG
 figure;
@@ -50,6 +57,11 @@ title('Predictions');
 sum(correctlabels==predictions)
 
 function [index, vec_distance] = assign_vector_to_centroid(data,centroids)
-% FILL IN
+  distances = zeros(size(centroids,1),1);
+  for i=1:size(centroids,1)
+    distances(i)=(norm(data-centroids(i,:)))^2; 
+    % Making an array of distances (squared norm) between vectors and centroids
+  end
+  [vec_distance, index] = min(distances);
 end
 
